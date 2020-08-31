136a1d7320858327002f4b3c49dfcc6deaafae00
Tanmay451 <tanmaypiyushkaushik451@gmail.com>
2020-08-31 22:59:02 +0530

    routes/routes.go: setup routes for index page

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,10 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"ams/handlers"
+
+	"github.com/gin-gonic/gin"
+)
 
 // InitializeRoutes will set up routes
 func InitializeRoutes() *gin.Engine {
@@ -8,8 +12,16 @@ func InitializeRoutes() *gin.Engine {
 	// Set the router as the default one provided by Gin
 	router := gin.Default()
 
+	// Process the templates at the start so that they don't have to be loaded
+	// from the disk again. This makes serving HTML pages very fast.
+	router.LoadHTMLGlob("templates/*")
+
 	// Use the setUserStatus middleware for every route to set a flag
 	// indicating whether the request was from an authenticated user or not
 	router.Use(setUserStatus())
+
+	// Handle the index route
+	router.GET("/", handlers.ShowIndexPage)
+
 	return router
 }
