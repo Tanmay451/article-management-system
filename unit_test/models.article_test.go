0100641b3b64f1d110fff5251684eb23589f80b0
Tanmay451 <tanmaypiyushkaushik451@gmail.com>
2020-09-04 00:25:15 +0530

    unit_test/models.article_test.go: unit test for model structure of article

---
+++ unit_test/models.article_test.go
@@ -0,0 +1,28 @@
+package unit_test
+
+import (
+	"ams/models"
+	"testing"
+)
+
+// Test the function that fetches all articles
+func TestGetAllArticles(t *testing.T) {
+	alist := models.GetAllArticles()
+
+	// Check that the length of the list of articles returned is the
+	// same as the length of the global variable holding the list
+	if len(alist) != len(models.ArticleList) {
+		t.Fail()
+	}
+
+	// Check that each member is identical
+	for i, v := range alist {
+		if v.Content != models.ArticleList[i].Content ||
+			v.ID != models.ArticleList[i].ID ||
+			v.Title != models.ArticleList[i].Title {
+
+			t.Fail()
+			break
+		}
+	}
+}
