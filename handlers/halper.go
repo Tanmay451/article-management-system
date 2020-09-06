0ab27cd6025645b4d23d9b987aae4e50f660e6a2
Tanmay451 <tanmaypiyushkaushik451@gmail.com>
2020-09-06 21:56:10 +0530

    handlers/halper.go: make halper function for user login

--- handlers/halper.go
+++ handlers/halper.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"ams/models"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +16,7 @@ func render(c *gin.Context, data gin.H, templateName string) {
 	loggedInInterface, _ := c.Get("is_logged_in")
 	data["is_logged_in"] = loggedInInterface.(bool)
 
+	// fmt.Println(c.Request.Header.Get("Accept"))
 	switch c.Request.Header.Get("Accept") {
 	case "application/json":
 		// Respond with JSON
@@ -24,4 +28,21 @@ func render(c *gin.Context, data gin.H, templateName string) {
 		// Respond with HTML
 		c.HTML(http.StatusOK, templateName, data)
 	}
+}
+
+// Check if the username and password combination is valid
+func isUserValid(username, password string) bool {
+	for _, u := range models.UserList {
+		if u.Username == username && u.Password == password {
+			return true
+		}
+	}
+	return false
+}
+
+func generateSessionToken() string {
+	// We're using a random 16 character string as the session token
+	// This is NOT a secure way of generating session tokens
+	// DO NOT USE THIS IN PRODUCTION
+	return strconv.FormatInt(rand.Int63(), 16)
 }
